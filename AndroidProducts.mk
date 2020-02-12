f5aec39ff0d2efbfd936fddc22aec4cb1bf82019
YuvaRaj-BT <yuvarajv952@gmail.com>
2020-02-12 14:32:36 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/ion_lavender.mk
+    $(LOCAL_DIR)/havoc_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    ion_lavender-user \
-    ion_lavender-userdebug \
-    ion_lavender-eng
+    havoc_lavender-user \
+    havoc_lavender-userdebug \
+    havoc_lavender-eng
